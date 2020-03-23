d626e05e0d94b147f006b46479b1a9a2261dc421
Ábel Kocsis <abelkocsis@cc.inf.elte.hu>
2020-03-23 18:36:14 +0100

    setup works with debian

--- setting-up-environment/starting.sh
+++ setting-up-environment/starting.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-cd /myvol/codechecker
+
+
+cd /testDir
+git clone https://github.com/Ericsson/codechecker.git
+cd /testDir/codechecker
+git checkout remotes/origin/release-v6.11.1
 
 make venv
 . $PWD/venv/bin/activate
 make package
 export PATH="$PWD/build/CodeChecker/bin:$PATH"
 
-cd /myvol/curl
+cd /testDir/curl
 ./buildconf
 ./configure
 
-codeChecker=/myvol/codechecker/build/CodeChecker/bin/CodeChecker
+codeChecker=/testDir/codechecker/build/CodeChecker/bin/CodeChecker
 
 $codeChecker log -b "make -j42" -o compilation.json
